3447532ba65386976a877df775d964d86e5bca88
Stephen Mulcahy <stephen.mulcahy@hp.com>
2013-11-27 11:04:43 +0000

    Fix string representation of VolumeBackupsRestore
    Change string representation of VolumeBackupsRestore to use
    volume_id rather than non-existent VolumeBackupsRestore id.
    
    Change-Id: I7b585d0de9b3b0511e7fd1a70b214090a17dfa93
    Closes-Bug: #1255488

--- cinderclient/v1/volume_backups_restore.py
+++ cinderclient/v1/volume_backups_restore.py
@@ -24,7 +24,7 @@ from cinderclient import base
 class VolumeBackupsRestore(base.Resource):
     """A Volume Backups Restore represents a restore operation."""
     def __repr__(self):
-        return "<VolumeBackupsRestore: %s>" % self.id
+        return "<VolumeBackupsRestore: %s>" % self.volume_id
 
 
 class VolumeBackupRestoreManager(base.Manager):
