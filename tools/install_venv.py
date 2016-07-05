00e942ed615bb9ceebc13dbe3fae2531f33e7285
yuyafei <yu.yafei@zte.com.cn>
2016-07-05 18:13:38 +0800

    Remove white space between print and ()
    TrivialFix
    
    Change-Id: I7cc4dc71459c9cfb2f45d9b3cb551fc3d6549ddb

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -43,7 +43,7 @@ def print_help(project, venv, root):
 
     $ %(root)s/tools/with_venv.sh <your command>
     """
-    print (help % dict(project=project, venv=venv, root=root))
+    print(help % dict(project=project, venv=venv, root=root))
 
 
 def main(argv):
