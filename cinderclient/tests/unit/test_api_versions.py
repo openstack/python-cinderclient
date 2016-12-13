61faea2eb3a3725e4dc81e15c1a402f2c8dc8fcc
scottda <scott.dangelo@gmail.com>
2016-12-13 12:14:26 -0700

    Add get_highest_version method
    Add method to query a server that supports /v3 endpoint and get the
    highest supported microversion.
    
    Change-Id: If179760787526440c852803eafaf9617bcd2d36e

--- cinderclient/tests/unit/test_api_versions.py
+++ cinderclient/tests/unit/test_api_versions.py
@@ -253,3 +253,9 @@ class DiscoverVersionTestCase(utils.TestCase):
                              discovered_version.get_string())
             self.assertTrue(
                 self.fake_client.services.server_api_version.called)
+
+    def test_get_highest_version(self):
+        self._mock_returned_server_version("3.14", "3.0")
+        highest_version = api_versions.get_highest_version(self.fake_client)
+        self.assertEqual("3.14", highest_version.get_string())
+        self.assertTrue(self.fake_client.services.server_api_version.called)
