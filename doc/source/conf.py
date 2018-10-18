004be70b44cd77df84dcd57848d20c61edc983ab
Eric Harney <eharney@redhat.com>
2018-10-18 16:00:30 -0400

    Fix doc build error
    Doc build keeps failing w/
    
    Recursion error:
        maximum recursion depth exceeded while pickling an object
    
    Sphinx recommends bumping up the recursion limit to
    get around this, and it appears to work.
    
    Change-Id: If0a78f9af2e4ddd5fd88c44757642355b3a5bf2e
    (cherry picked from commit 2505e2a9031f13fbcb1f639ee5f21a2a30f77715)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 import pbr.version
 
+sys.setrecursionlimit(4000)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
