44bcd69f5a34905a63c5fa849bcfbee7b73fd56d
Sean McGinnis <sean.mcginnis@gmail.com>
2019-04-17 09:28:50 -0500

    Raise API max version for Rocky updates
    Functionality was added during rocky, but the MAX_VERSION of the client
    was not updated for these versions. This raises the version to support
    the added functionality.
    
    3.53 - Schema validation - no client changes
    3.54 - Add mode option to attachment-create:
        I22cfddd0192c4a72b8f844f23d1fa51b96c57e06
    3.55 - Transfer snapshots with volumes
        I61a84b5abf386a4073baea57d8820c8fd762ae03
    
    This patch adds a release note to the cherry pick indicated below.
    
    Change-Id: I8c8fb8f552e529c8474a3e3b771ba8eb7aed193a
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>
    (cherry picked from commit b843a168efc9261a12ebe322fe701ef355474874)
    (cherry picked from commit ee37f267354a8dfe5d4b9b05c261bf86f329cfb8)

--- cinderclient/api_versions.py
+++ cinderclient/api_versions.py
@@ -29,7 +29,7 @@ LOG = logging.getLogger(__name__)
 # key is a deprecated version and value is an alternative version.
 DEPRECATED_VERSIONS = {"1": "2"}
 DEPRECATED_VERSION = "2.0"
-MAX_VERSION = "3.52"
+MAX_VERSION = "3.55"
 MIN_VERSION = "3.0"
 
 _SUBSTITUTIONS = {}
